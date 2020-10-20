ca6aa18ee0c524253ef03610c072991a52f8f125
FlaviusMihailescu <flaviusmihailescu30@gmail.com>
2020-10-20 21:30:05 +0300

    Commited

---
+++ Tema4/Question.domain4/Question.domain4/PostedQuestionWorkflow/PostedQuestionResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question.domain4.PostedQuestionWorkflow
+{
+    public static partial class PostedQuestionResult
+    {
+        public interface IPostedQuestion
+        {
+        }
+
+        public class QuestionPosted : IPostedQuestion
+        {
+            public QuestionPosted(int id)
+            {
+                Id = id;
+            }
+
+            public int Id { get; }
+        }
+
+        public class QuestionNotPosted : IPostedQuestion
+        {
+            public QuestionNotPosted(string errorMessage)
+            {
+                ErrorMessage = errorMessage;
+            }
+
+            public string ErrorMessage { get; }
+        }
+        public class InvalidQuestion : IPostedQuestion
+        {
+            public InvalidQuestion(PostedQuestionCmd cmd)
+            {
+                Cmd = cmd;
+            }
+
+            public PostedQuestionCmd Cmd { get; }
+        }
+
+
+    }
+}
